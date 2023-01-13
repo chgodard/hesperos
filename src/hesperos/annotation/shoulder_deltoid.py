b71d6fae7f5dd82e52d9ddeb980d404a797b595f
cgodard <charlotte.godard@pasteur.fr>
2023-01-13 14:42:59 +0100

    remove

--- src/hesperos/annotation/shoulder_deltoid.py
+++ src/hesperos/annotation/shoulder_deltoid.py
@@ -2,16 +2,11 @@
 
 LIST_STRUCTURES = [
     "DELTOID",
-    # "OTHER",
 ]
 
 DICT_SUB_STRUCTURES = {
     "DELTOID":[
         'Anterior',
         'Lateral',
-        'Posterior',],
-    
-    # "OTHER":[
-    #     'Other',
-    #     ]
+        'Posterior']
 }
